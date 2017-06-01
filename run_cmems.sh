128d6d7ed3242e7ebed723f84e1ffd27783aa2b3
Ilias Ioannou <ioannou@planetek.gr>
2017-06-01 10:11:40 +0200

    Added processors service to compose file and update docs

--- run_cmems.sh
+++ run_cmems.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
 
-echo "Running compose "
+set -e
+RED='\033[0;31m'
+NC='\033[0m'
+
+function build_cmems_processors {
+  printf "${RED}Building cmems_processors${NC}\n"
+  git clone -b develop https://tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Processori tmp
+  cd tmp/Processors/docker
+  sh build.sh develop
+  cd ../../..
+  rm -rf tmp
+}
+
+
+build_cmems_processors
+docker-compose down
+printf "${RED}Running compose${NC}\n"
 docker-compose up -d postgres
-echo "Going to sleep"
+printf "${RED}Going to sleep${NC}\n"
 sleep 10
 docker-compose up -d
