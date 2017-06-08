350dae116fa3af1907c4f7df66f4bdcf73d95e7c
Ilias Ioannou <ioannou@planetek.gr>
2017-06-08 17:59:18 +0200

    Added geoserver/geonetwork container to compose file

--- run_cmems.sh
+++ run_cmems.sh
@@ -4,17 +4,50 @@ set -e
 RED='\033[0;31m'
 NC='\033[0m'
 
+function build_cmems_geoserver {
+  printf "${RED}Preparing geoserver instance${NC}\n"
+  git clone -b develop https://teamEreticoTfs:vAsK*AIJFB@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_MapServices tmp
+  cd tmp
+  
+  if [ ! -d "../shared_data/geoserver" ]; then
+    mkdir ../shared_data/geoserver
+    mv *  ../shared_data/geoserver
+  fi
+
+  cd ..
+  rm -rf tmp
+}
+
 function build_cmems_processors {
   printf "${RED}Building cmems_processors${NC}\n"
-  git clone -b develop https://tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Processori tmp
+  git clone -b develop https://teamEreticoTfs:vAsK*AIJFB@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Processori tmp
   cd tmp/Processors/docker
   sh build.sh develop
   cd ../../..
   rm -rf tmp
 }
 
+function build_cmems_geonetwork {
+  printf "${RED}Preparing geonetwork${NC}\n"
+  git clone -b develop https://teamEreticoTfs:vAsK*AIJFB@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_connettore_catalogo tmp
+  cd tmp
+  
+  if [ ! -d "../shared_data/postgres/datavolume" ]; then
+    mv datavolume ../shared_data/postgres/datavolume
+  fi
+
+  if [ ! -d "../shared_data/postgres/data" ]; then
+    mkdir ../shared_data/postgres/data
+  fi
+
+  cd ..
+  rm -rf tmp
+}
+
 
 build_cmems_processors
+build_cmems_geoserver
+build_cmems_geonetwork
 docker-compose down
 printf "${RED}Running compose${NC}\n"
 docker-compose up -d postgres
