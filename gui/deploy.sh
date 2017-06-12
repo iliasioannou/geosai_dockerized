820c7041ade9f8ebdba7cbc558eab08fde828963
Maurizio Cosmai <cosmai@planetek.it>
2017-06-12 17:25:59 +0200

    Deleted gui folder

--- gui/deploy.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-docker build -t planetek/node-bower-compass-grunt:4 -f Dockerfile.base .
-docker build --no-cache -t planetek/cmems-gui:$1 --build-arg branch=$1 .
-#docker-compose up -d cmems_gui
