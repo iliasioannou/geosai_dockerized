29b0e136d4b9743606c44a89b4e91eda6cc26d69
Ilias Ioannou <ioannou@planetek.gr>
2017-06-19 16:45:22 +0200

    removed exposed servicex outside and added activiti service

--- run_cmems.sh
+++ run_cmems.sh
@@ -5,8 +5,11 @@ set -e
 RED='\033[0;31m'
 NC='\033[0m'
 
-printf "${RED}Removing old tmp folder${NC}\n"
-rm -rf tmp
+function clean {
+  printf "${RED}Removing old tmp folder${NC}\n"
+  rm -rf tmp  
+}
+
 
 function build_cmems_esb {
   printf "${RED}Preparing Mule ESB instance${NC}\n"
@@ -61,14 +64,32 @@ function build_cmems_gui {
   rm -rf tmp
 }
 
+function build_cmems_activiti {
+  printf "${RED}Importing data in Activiti db${NC}\n"
+  git clone -b develop https://teamEreticoTfs:vAsK*AIJFB@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_WFE  tmp
+  cd tmp/docker
+  sh build.sh develop 
+  cd ../..
+  rm -rf tmp
+}
+
+clean
 build_cmems_processors
 build_cmems_gui
 build_cmems_geoserver
 build_cmems_geonetwork
 build_cmems_esb
-#docker-compose down
+build_cmems_activiti
+docker-compose down
 printf "${RED}Running compose${NC}\n"
-#docker-compose up -d postgres
+docker-compose up -d postgres mysql
+build_cmems_activiti
 printf "${RED}Going to sleep${NC}\n"
-#sleep 10
-#docker-compose up -d
+sleep 10
+docker-compose up -d
+sleep 10
+docker exec cmems_activiti bash -c '/src/load.sh'
+docker exec -t -i cmems_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
+docker exec -t -i cmems_geonetwork bash -c "sed -i -- 's/port=\"8080\"/port=\"9080\"/g' /usr/local/tomcat/conf/server.xml"
+docker exec -t -i cmems_geonetwork bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /usr/local/tomcat/conf/server.xml
+docker-compose restart geoserver geonetwork activiti
