30152b3518e43e1843061a8a2b8467e8f21143c5
Docker manager <dockermanager@temisto.planetek.it>
2017-07-07 11:24:31 +0200

    Removed special char from git clone url

--- run_cmems.sh
+++ run_cmems.sh
@@ -95,23 +95,23 @@ function build_cmems_api {
 }
 
 clean
-build_cmems_processors
-build_cmems_gui
-build_cmems_geoserver
-build_cmems_geonetwork
-build_cmems_esb
-build_cmems_activiti
+#build_cmems_processors
+#build_cmems_gui
+#build_cmems_geoserver
+#build_cmems_geonetwork
+#build_cmems_esb
+#build_cmems_activiti
 build_cmems_manager
 build_cmems_api
-docker-compose down
+#docker-compose down
 printf "${RED}Running compose${NC}\n"
-docker-compose up -d postgres mysql mysql_manager
+#docker-compose up -d postgres mysql mysql_manager
 printf "${RED}Going to sleep${NC}\n"
-sleep 10
+#sleep 10
 docker-compose up -d
-sleep 10
-docker exec cmems_activiti bash -c '/src/load.sh'
-docker exec -t -i cmems_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
+#sleep 10
+#docker exec cmems_activiti bash -c '/src/load.sh'
+#docker exec -t -i cmems_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
 #docker exec -t -i cmems_geonetwork bash -c "sed -i -- 's/port=\"8080\"/port=\"9080\"/g' /usr/local/tomcat/conf/server.xml"
-docker exec -t -i cmems_activiti  bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /opt/tomcat/conf/server.xml"
-docker-compose restart geoserver geonetwork activiti
+#docker exec -t -i cmems_activiti  bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /opt/tomcat/conf/server.xml"
+#docker-compose restart geoserver geonetwork activiti
