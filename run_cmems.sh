dab20150a320af251473403316fe8799887ac78b
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 17:27:12 +0100

    Updated run_cmems.sh

--- run_cmems.sh
+++ run_cmems.sh
@@ -11,21 +11,21 @@ function clean {
 }
 
 
-function build_cmems_esb {
+function build_eosai_esb {
   printf "${RED}Preparing Mule ESB instance${NC}\n"
   if [ ! -d "shared_data/download" ]; then
     mkdir shared_data/download
   fi
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_ESB tmp
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_ESB tmp
   cd tmp/ESB/docker
   sh build.sh master
   cd ../../..
   rm -rf tmp
 }
 
-function build_cmems_geoserver {
+function build_eosai_geoserver {
   printf "${RED}Preparing geoserver instance${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_MapServices tmp
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_MapServices tmp
   mkdir config
   cp tmp/config/users.xml config
   cd tmp
@@ -40,9 +40,9 @@ function build_cmems_geoserver {
   rm -rf config
 }
 
-function build_cmems_processors {
-  printf "${RED}Building cmems_processors${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Processori tmp
+function build_eosai_processors {
+  printf "${RED}Building eosai_processors${NC}\n"
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_Processori tmp
   cd tmp/Processors/docker
   sh build.sh master
   cd ../../..
@@ -50,9 +50,9 @@ function build_cmems_processors {
 }
 
 
-function build_cmems_geonetwork {
+function build_eosai_geonetwork {
   printf "${RED}Preparing geonetwork${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_connettore_catalogo tmp
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_connettore_catalogo tmp
   cd tmp
   if [ ! -d "../shared_data/postgres/datavolume" ]; then
     mv datavolume ../shared_data/postgres
@@ -61,58 +61,58 @@ function build_cmems_geonetwork {
   rm -rf tmp
 }
 
-function build_cmems_gui {
-  printf "${RED}Building cmem_gui${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Web_App tmp
-  cd tmp/cmems-gui/docker
+function build_eosai_gui {
+  printf "${RED}Building eosai_gui${NC}\n"
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_Web_App tmp
+  cd tmp/eosai-gui/docker
   sh deploy.sh master
   cd ../../..
   rm -rf tmp
 }
 
-function build_cmems_activiti {
-  printf "${RED}Building cmems_activiti${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_WFE  tmp
+function build_eosai_activiti {
+  printf "${RED}Building eosai_activiti${NC}\n"
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_WFE  tmp
   cd tmp/docker
   sh build.sh master
   cd ../..
   rm -rf tmp
 }
 
-function build_cmems_manager {
-  printf "${RED}Building cmems_manager${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Manager_OD_Request  tmp
+function build_eosai_manager {
+  printf "${RED}Building eosai_manager${NC}\n"
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_Manager_OD_Request  tmp
   cd tmp/docker
   sh build.sh master
   cd ../..
   rm -rf tmp
 }
 
-function build_cmems_api {
-  printf "${RED}Building cmems_api${NC}\n"
-  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/SBU-GS/pkz029_UU_CMEMS/_git/pkz029_UU_CMEMS_Api_Rest  tmp
-  cd tmp/cmems-api/docker
+function build_eosai_api {
+  printf "${RED}Building eosai_api${NC}\n"
+  git clone -b master https://teamEreticoTfs:hIEMK-i=d@tfs.planetek.it/Planetek%20Hellas/pkh111_EOSAI/_git/pkh111_EOSAI_Api_Rest  tmp
+  cd tmp/eosai-api/docker
   sh deploy.sh master
   cd ../../..
   rm -rf tmp
 }
 
 function build_proxy {
-  printf "${RED}Building cmems_proxy${NC}\n"
+  printf "${RED}Building eosai_proxy${NC}\n"
   cd proxy
-  docker build -t planetek/cmems_proxy:base .
+  docker build -t planetek/eosai_proxy:base .
   cd ..
 }
 
 clean
-build_cmems_processors
-build_cmems_gui
-build_cmems_geoserver
-build_cmems_geonetwork
-build_cmems_esb
-build_cmems_activiti
-build_cmems_manager
-build_cmems_api
+build_eosai_processors
+build_eosai_gui
+build_eosai_geoserver
+build_eosai_geonetwork
+build_eosai_esb
+build_eosai_activiti
+build_eosai_manager
+build_eosai_api
 build_proxy
 docker-compose down
 printf "${RED}Running compose${NC}\n"
@@ -122,7 +122,7 @@ sleep 10
 docker exec -it cmems_postgres psql -U postgres geonetwork -a -f opt/data-volume/db.sql
 docker-compose up -d
 sleep 10
-docker exec cmems_activiti bash -c '/src/load.sh'
-docker exec -t -i cmems_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
-docker exec -t -i cmems_activiti  bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /opt/tomcat/conf/server.xml"
+docker exec eosai_activiti bash -c '/src/load.sh'
+docker exec -t -i eosai_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
+docker exec -t -i eosai_activiti  bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /opt/tomcat/conf/server.xml"
 docker-compose restart geoserver geonetwork activiti
