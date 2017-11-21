102d6b9164cd5981038f483b40af52cfba52932c
Ilias Ioannou <ioannou@planetek.gr>
2017-11-21 17:31:24 +0100

    Updated init_wordpress_backup_container.sh

--- init_wordpress_backup_container.sh
+++ init_wordpress_backup_container.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 docker run \
-  --net cmems \
-  --name cmems_wordpress_backup \
+  --net eosai \
+  --name eosai_wordpress_backup \
   -v `pwd`/shared_data/backups:/backups \
-  --volumes-from=cmems_wordpress \
-  --link=cmems_wordpress_mysql:mysql \
+  --volumes-from=eosai_wordpress \
+  --link=eosai_wordpress_mysql:mysql \
   -d aveltens/wordpress-backup
