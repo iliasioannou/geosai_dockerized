4a9701d03aee5731ed09f29b3b54ab23bbfe21a4
Ilias Ioannou <ioannou@planetek.gr>
2017-11-27 09:11:33 +0100

    Deleted /init_wordpress_backup_container.sh

--- init_wordpress_backup_container.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker run \
-  --net eosai \
-  --name eosai_wordpress_backup \
-  -v `pwd`/shared_data/backups:/backups \
-  --volumes-from=eosai_wordpress \
-  --link=eosai_wordpress_mysql:mysql \
-  -d aveltens/wordpress-backup
