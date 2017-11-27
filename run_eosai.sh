ba6379e532c911860afeb3c18759ebb7395501e4
Ilias Ioannou <ioannou@planetek.gr>
2017-11-27 11:31:52 +0100

    Updated run_eosai.sh

--- run_eosai.sh
+++ run_eosai.sh
@@ -111,6 +111,3 @@ clean
 docker-compose up -d
 #sleep 10
 #docker exec eosai_activiti bash -c '/src/load.sh'
-# docker exec -t -i eosai_geoserver bash -c "sed -i -- 's/port=\"8080\"/port=\"9090\"/g' /usr/local/tomcat/conf/server.xml"
-# docker exec -t -i eosai_activiti  bash -c "sed -i -- 's/port=\"8080\"/port=\"9085\"/g' /opt/tomcat/conf/server.xml"
-# docker-compose restart geoserver geonetwork activiti
